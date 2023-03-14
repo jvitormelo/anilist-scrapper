6ef28dd6ddb6906efd3c3089f28a6be718f0c439
jvitormelo <jvitor.ovidio2@gmail.com>
2023-03-14 17:30:34 -0300

    executting qbittorent

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use scraper::{Html, Selector};
 use serde::{Deserialize, Serialize};
 use serde_json;
 use std::fs;
+use std::process::Command;
 
 #[derive(Serialize, Deserialize)]
 struct Config {
@@ -58,6 +60,11 @@ struct Anime {
     media: Media,
 }
 
+struct Torrent {
+    magnetic: String,
+    name: String,
+}
+
 #[tokio::main]
 async fn main() {
     if !path_exists(CONFIG_PATH) {
@@ -101,12 +108,86 @@ async fn main() {
         }
     }
 
-    to_watch.iter().for_each(|anime| {
-        println!("{} - {}", anime.media.title.romaji, anime.progress);
-    });
+    let torrents = scrap_animes(to_watch).await;
+
+    for torrent in torrents {
+        println!("Downloading {}", torrent.name);
+        start_qbittorrent(
+            &_json["qbittorrent_path"].as_str().unwrap(),
+            &_json["save_path"].as_str().unwrap(),
+            &torrent.magnetic,
+        );
+    }
+
+    println!("Have fun!");
+
+    std::process::exit(0);
 }
 
-fn scrap_animes(animes: Vec<Anime>) {}
+fn start_qbittorrent(path: &str, save_path: &str, magnetic: &str) {
+    let mut child = Command::new(path)
+        .arg("--skip-dialog")
+        .arg(&magnetic)
+        .spawn();
+
+    match child {
+        Ok(mut child) => {
+            child.wait().unwrap();
+        }
+        Err(e) => println!("failed to execute process: {}", e),
+    }
+}
+
+async fn scrap_animes(animes: Vec<Anime>) -> Vec<Torrent> {
+    let mut magnetics: Vec<Torrent> = vec![];
+
+    for anime in animes {
+        let next_episode = anime.progress + 1;
+        let anime_name = anime.media.title.romaji + " - " + &next_episode.to_string();
+
+        let encoded_name = urlencoding::encode(&anime_name);
+
+        let url = format!(
+            "https://nyaa.si/?f=0&c=1_2&q={}&s=seeders&o=desc",
+            encoded_name
+        );
+
+        let client = reqwest::Client::new();
+
+        println!("Scraping {}", url);
+        let resp = client.get(&url).send().await;
+
+        let body = resp.unwrap().text().await.unwrap();
+
+        let document = Html::parse_document(&body);
+
+        let selector = Selector::parse("tbody > tr:first-child").unwrap();
+
+        for element in document.select(&selector) {
+            let selector = Selector::parse("td:nth-child(3) > a:nth-child(2)").unwrap();
+
+            let name_selector = Selector::parse("td:nth-child(2) > a:last-child").unwrap();
+
+            let magnetic = element
+                .select(&selector)
+                .next()
+                .unwrap()
+                .value()
+                .attr("href")
+                .unwrap();
+
+            let name = element.select(&name_selector).next().unwrap().inner_html();
+
+            magnetics.push(Torrent {
+                magnetic: magnetic.to_string(),
+                name: name.to_string(),
+            });
+        }
+    }
+
+    println!("\nFinished Scrapping \n",);
+    return magnetics;
+}
 
 async fn get_watching_animes(anilist_user_id: i32) -> Vec<Anime> {
     let client = reqwest::Client::new();
@@ -119,6 +200,7 @@ async fn get_watching_animes(anilist_user_id: i32) -> Vec<Anime> {
         query: QUERY.to_string(),
     };
 
+    println!("Getting Watching List for {} \n", anilist_user_id);
     let resp = client
         .post("https://graphql.anilist.co/")
         .json(&body)
